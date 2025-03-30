d386d0ccf73b384a243a4e3e489077969bfa7047
jaornelas <ornelalf001@gmail.com>
2025-03-30 16:49:04 -0500

    complete createThought function in controller

--- src/controllers/thought-controller.ts
+++ src/controllers/thought-controller.ts
@@ -1,8 +1,8 @@
-import { Reqest, Response } from 'express';
+import { Request, Response } from 'express';
 import { Thought, User } from '../models/index.js'
 
 //get all thoughts
-export default getThoughts =  async (_req: Request, res: Response) => {
+export const getThoughts =  async (_req: Request, res: Response) => {
     try {
         const dbThoughtData = await Thought.find()
         .sort({ createdAt: -1 });
@@ -15,31 +15,59 @@ export default getThoughts =  async (_req: Request, res: Response) => {
 }
 
 //get single thought by ID
-export const getSingleThought = async (_req: Request, res: Response) => {
+export const getSingleThought = async (req: Request, res: Response) => {
+    try {
+        const dbThoughtData = await Thought.findOne({ _id: req.params.thoughtId });
 
+        if (!dbThoughtData) {
+            return res.status(404).json({ message: 'No thoughts with this ID' });
+        }
+
+        return res.json(dbThoughtData);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
 }
 
 //create thought 
-export const createThought = async (_req: Request, res: Response) => {
+export const createThought = async (req: Request, res: Response) => {
+    try {
+        const dbThoughtData = await Thought.create(req.body);
 
+        const dbUserData = await User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $push: { thoughts: dbThoughtData._id }},
+            { new: true }
+        );
+
+        if (!dbUserData) {
+            return res.status(404).json({ message: 'Thought created but could not find user with this ID'});
+        }
+
+        return res.json({ message: 'Thought successfully created!' });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
 }
 
 //update thought
-export const updateThought = async (_req: Request, res: Response) => {
+export const updateThought = async (req: Request, res: Response) => {
 
 }
 
 //delete thought 
-export const deleteThought = async (_req: Request, res: Response) => {
+export const deleteThought = async (req: Request, res: Response) => {
 
 }
 
 //add reaction to thought 
-export const addReaction = async (_req: Request, res: Response) => {
+export const addReaction = async (req: Request, res: Response) => {
 
 }
 
 //remove reaction from thought
-export const removeReaction = async (_req: Request, res: Response) => {
+export const removeReaction = async (req: Request, res: Response) => {
 
 }
