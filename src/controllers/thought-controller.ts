df7d27f7694720d80a9bf5400a15328b5cffefbc
jaornelas <ornelalf001@gmail.com>
2025-03-30 16:58:52 -0500

    finish up the thoughts controller

--- src/controllers/thought-controller.ts
+++ src/controllers/thought-controller.ts
@@ -2,12 +2,12 @@ import { Request, Response } from 'express';
 import { Thought, User } from '../models/index.js'
 
 //get all thoughts
-export const getThoughts =  async (_req: Request, res: Response) => {
+export const getThoughts = async (_req: Request, res: Response) => {
     try {
         const dbThoughtData = await Thought.find()
-        .sort({ createdAt: -1 });
+            .sort({ createdAt: -1 });
 
-        return res.json (dbThoughtData);
+        return res.json(dbThoughtData);
     } catch (err) {
         console.log(err);
         return res.status(500).json(err);
@@ -37,12 +37,12 @@ export const createThought = async (req: Request, res: Response) => {
 
         const dbUserData = await User.findOneAndUpdate(
             { _id: req.body.userId },
-            { $push: { thoughts: dbThoughtData._id }},
+            { $push: { thoughts: dbThoughtData._id } },
             { new: true }
         );
 
         if (!dbUserData) {
-            return res.status(404).json({ message: 'Thought created but could not find user with this ID'});
+            return res.status(404).json({ message: 'Thought created but could not find user with this ID' });
         }
 
         return res.json({ message: 'Thought successfully created!' });
@@ -54,20 +54,83 @@ export const createThought = async (req: Request, res: Response) => {
 
 //update thought
 export const updateThought = async (req: Request, res: Response) => {
-
-}
+    try {
+        const dbThoughtData = await Thought.findOneAndUpdate({ _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true });
+        if (!dbThoughtData) {
+            return res.status(404).json({ message: 'No thought with this ID' });
+        }
+        return res.json(dbThoughtData);
+    } catch (err: any) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+};
 
 //delete thought 
 export const deleteThought = async (req: Request, res: Response) => {
+    try {
+        const dbThoughtData = await Thought.findOneAndDelete({ _id: req.params.thoughtId })
 
+        if (!dbThoughtData) {
+            return res.status(404).json({ message: 'No thought with this id!' });
+        }
+
+        // remove thought id from user's `thoughts` field
+        const dbUserData = User.findOneAndUpdate(
+            { thoughts: req.params.thoughtId },
+            { $pull: { thoughts: req.params.thoughtId } },
+            { new: true }
+        );
+
+        if (!dbUserData) {
+            return res.status(404).json({ message: 'Thought created but could not find user with this ID' });
+        }
+
+        return res.json({ message: 'Thought successfully deleted!' });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
 }
 
 //add reaction to thought 
 export const addReaction = async (req: Request, res: Response) => {
+    try {
+        const dbThoughtData = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+        );
 
+        if (!dbThoughtData) {
+            return res.status(404).json({ message: 'No thought with this ID' });
+        }
+
+        return res.json(dbThoughtData);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
 }
 
 //remove reaction from thought
 export const removeReaction = async (req: Request, res: Response) => {
+    try {
+        const dbThoughtData = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+        );
 
+        if (!dbThoughtData) {
+            return res.status(404).json({ message: 'No thought with this ID' });
+        }
+
+        return res.json(dbThoughtData);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
 }
