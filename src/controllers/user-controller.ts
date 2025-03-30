fa6f6a4e0c931f7b4a99df72eb126811585172fa
jaornelas <ornelalf001@gmail.com>
2025-03-30 15:38:10 -0500

    layout user-controller

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { User, Thought } from '../models/index.js';
+
+//get all users
+export const getUsers = async (_req: Request, res: Response) => {
+
+}
+
+//get single user by ID
+export const getSingleUser = async (_req: Request, res: Response) => {
+
+}
+
+//create a new user
+export const createNewUser = async (_req: Request, res: Response) => {
+
+}
+
+//update a user
+export const updateUser = async (_req: Request, res: Response) => {
+
+}
+
+//delete a user
+export const deleteUsers = async (_req: Request, res: Response) => {
+
+}
+
+//add a friend to friend list
+export const addFriend = async (_req: Request, res: Response) => {
+
+}
+
+//remove friend from friend list
+export const removeFriend = async (_req: Request, res: Response) => {
+
+}
