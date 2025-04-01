84386ae0134af66bed267ecb088f85d9209fc959
jaornelas <ornelalf001@gmail.com>
2025-04-01 11:59:41 -0500

    start working on user controller

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -3,22 +3,63 @@ import { User, Thought } from '../models/index.js';
 
 //get all users
 export const getUsers = async (_req: Request, res: Response) => {
+    try {
+        const dbUserData = await User.find()
+            .select('-__v')
 
+        return res.json(dbUserData)
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
 }
 
 //get single user by ID
-export const getUserByID = async (_req: Request, res: Response) => {
+export const getUserByID = async (req: Request, res: Response) => {
+    try {
+        const dbUserData = await User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .populate('friends')
+            .populate('thoughts');
 
+        if (!dbUserData) {
+            return res.status(404).json({ message: 'No user with this ID' })
+        }
+    }
 }
 
 //create a new user
 export const createNewUser = async (_req: Request, res: Response) => {
-
+    try {
+        const dbUserData = await User.create(req.body);
+        return res.json(dbUserData)
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err)
+    }
 }
 
 //update a user
-export const updateUser = async (_req: Request, res: Response) => {
+export const updateUser = async (req: Request, res: Response) => {
+    try {
+        const dbUserData = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            {
+                runValidators: true,
+                new: true,
+            }
+        );
 
+        if (!dbUserData) {
+            return res.status(404).json({ message: 'No users with this ID' });
+        }
+
+        return res.json(dbUserData);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err)
+    }
 }
 
 //delete a user
