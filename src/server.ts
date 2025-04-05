194be88f860b48f5641912ca8d19e10196394af5
jaornelas <ornelalf001@gmail.com>
2025-04-05 14:56:36 -0500

    fix routes directory in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes/api/index.js';
+import routes from './routes/index.js';
 import db from './config/connection.js';
 
 await db();
@@ -11,6 +11,10 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.use(routes);
+app.use((req, _res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
